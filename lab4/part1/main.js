84bbbd8b7222ff80d07b7a8321749d41bba94845
stradlingm <matthew.stradling@dcmail.ca>
2025-06-15 12:40:16 -0400

    Update main.js
    added result function, added constants for xitem, yitem, and zitem. added replace methods for insertx, inserty, and insertz

--- lab4/part1/main.js
+++ lab4/part1/main.js
@@ -15,3 +15,32 @@ const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 const insertY = ["the soup kitchen", "Disneyland", "the White House"];
 
 const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+    let newStory = storyText;
+
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
+
+    newStory = newStory.replace(":insertx:", xItem);
+    newStory = newStory.replace(":inserty:", yItem);
+    newStory = newStory.replace(":insertz:", zItem);
+
+  if(customName.value !== '') {
+    const name = customName.value;
+    
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
