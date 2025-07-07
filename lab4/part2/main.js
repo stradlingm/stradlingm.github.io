bf31f8834018e31e2c96f28e65d788f5c97a59ff
MStradling17 <matthew.stradling@dcmail.ca>
2025-07-07 13:36:30 -0400

    Update main.js
    Added array for image filenames, an alternate text object for  each images alternate text

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -5,9 +5,15 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const imageArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 /* Declaring the alternative text for each image file */
-
+const altText = {
+    'pic1.jpg' : "Zoomed-in picture of a persons eye",
+    'pic2.jpg' : "Wavy-Brown artpiece",
+    'pic3.jpg' : "A field of purple and white flowers",
+    'pic4.jpg' : "Wall art of Egyptian Pharaoahs",
+    'pic5.jpg' : "A giant moth on a leaf"
+};
 /* Looping through images */
 
 const newImage = document.createElement('img');
