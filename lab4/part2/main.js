9e071dbbd23e82dd6f5a1b520eec601de68d2279
stradlingm <matthew.stradling@dcmail.ca>
2025-07-17 11:06:44 -0400

    Update main.js
    Added a pictureAltText object that holds an altText for each image file name

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -6,7 +6,15 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const pictureArray = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
-/* Declaring the alternative text for each image file */
+
+/* Declaring the alternative text for each image file */ 
+const pictureAltText = {
+    "pic1.jpg" : "Closeup of a human eye",
+    "pic2.jpg" : "Wavy brown portrait",
+    "pic3.jpg" : "Field of purple flowers",
+    "pic4.jpg" : "Wall painting of ancient egyptian pharoahs",
+    "pic5.jpg" : "Brown butterfly eating a leaf"
+}
 
 /* Looping through images */
 
