03dcbfa9369d544b47f7552d678757f4a256f86c
stradlingm <matthew.stradling@dcmail.ca>
2025-07-26 13:58:37 -0400

    Update main-finished.js
    Added the EvilCircle class that extends the Shape class, added the constructor for the EvilCircle class which includes a keydown event listener that moves the circle around the screen with wasd

--- lab4/part4/main-finished.js
+++ lab4/part4/main-finished.js
@@ -27,7 +27,7 @@ class Shape {
     }
 }
 
-class Ball extends Shape{
+class Ball extends Shape {
   constructor(x, y, velX, velY, color, size) {
     super(x, y, velX, velY);
     this.color = color;
@@ -75,6 +75,32 @@ class Ball extends Shape{
         }
       }
     }
+  }
+}
+
+// EvilCircle class that extends the shape class
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = "white";
+    this.size = 10;
+
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
   }
 }
 
