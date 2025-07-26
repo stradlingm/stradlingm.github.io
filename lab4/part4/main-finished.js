91197b7f6e99d120c3fb216280bdb601e7af1a08
stradlingm <matthew.stradling@dcmail.ca>
2025-07-26 13:49:52 -0400

    changed file name
    Added the exists property to the ball constructor, added ball.exists conditon to the collisionDetect() function---Changed file name from main.js to main-finished.js to fix an error with linking the js to the html file

--- lab4/part4/main-finished.js
+++ lab4/part4/main-finished.js
@@ -32,6 +32,7 @@ class Ball extends Shape{
     super(x, y, velX, velY);
     this.color = color;
     this.size = size;
+    this.exists = true;
   }
 
   draw() {
@@ -64,7 +65,7 @@ class Ball extends Shape{
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
